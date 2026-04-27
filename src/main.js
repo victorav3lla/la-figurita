2044c49c52ca448ec8d16ba843ecdc248c2d370f
Victor Avella <victorhugo.avellalemus@gmail.com>
2026-04-26 23:26:47 -0500

    Initial setup: Vite + Tailwind v4

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <main class="min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold">La Figurita</h1>
+  </main>
+`
